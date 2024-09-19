599694638343f87af15ca57037e89be1344a3434
amrrdev <amrrdev@gmail.com>
2024-09-19 18:33:48 +0300

    refactor: generalize the interceptor for any resource

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,13 +1,14 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { CallHandler, ExecutionContext, NestInterceptor, Type } from '@nestjs/common';
 import { plainToClass } from 'class-transformer';
 import { map, Observable } from 'rxjs';
 import { UserDto } from 'src/users/dto/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private DTO: Type<any>) {}
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       map((response: any) => {
-        return plainToClass(UserDto, response, {
+        return plainToClass(this.DTO, response, {
           excludeExtraneousValues: true,
         });
       }),
